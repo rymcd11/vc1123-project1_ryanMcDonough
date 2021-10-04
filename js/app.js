0a108444d7d4f6d39236e2df6d9184d1c3a7fff1
rymcd11 <beatingeverylevel@gmail.com>
2021-10-04 15:33:40 -0500

    testing java decisions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Ryan McDonough") 
+console.log("Ryan McDonough")
+
+let num1 = 25;
+let num2 = 30;
+let heading1 = document.getElementById('heading1');
+/*console.log('document1')*/
+
+if(null !== 25) {
+    heading1.innerHTML = "We have " + num1 + " days to go";
+}
+
+else{
+    heading1.innerHTML = "We don't have " + num2 + " days to go";
+
+}
